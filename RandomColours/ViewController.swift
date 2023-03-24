75f8c3b5508d72b3001cdce0ffb86b8ee2c7ab36
Dru Mumba <drusademumba@gmail.com>
2023-03-24 13:26:09 +0300

    Initial Commit

---
+++ RandomColours/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColours
+//
+//  Created by Dru Mumba on 24/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
